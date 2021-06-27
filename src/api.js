edb1261cd8ae9532a5c4152f64c49e542d50fa05
Misha Saidov <mavlyd.saidov@gmail.com>
2021-06-28 05:09:44 +0600

    new api logic

--- src/api.js
+++ src/api.js
@@ -1,157 +1,457 @@
 const mysql = require("mysql2")
-const log = require("./log")
+const config = require("./config")
 
 const DB = mysql.createPool({
-	host: "localhost",
-	user: "root",
-	database: "saved_img_bot"
+	host: config.MYSQL_HOST,
+	user: config.MYSQL_USER,
+	database: config.MYSQL_DATABASE
 })
 
-const insert = ({table, columns}) => {
-	return new Promise((resolve, reject) => {
-		DB.query(
-			`INSERT INTO \`${table}\` SET ${Object.keys(columns).map(item => `\`${item}\` = ?`).join(", ")}`,
-			Object.values(columns),
-			(error, results, fields) => {
-				if (error) {
-					log.red("Insert error:", {
-						table: table,
-						columns: columns,
-						error: error,
-					})
-					reject(error)
+
+const saveFile = ({
+	chat_id,
+	type,
+	file_size,
+	file_id,
+	file_unique_id,
+	height,
+	width,
+	tags = "",
+	file_message_id,
+	tags_message_id,
+	media_group_id = "",
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			INSERT INTO \`${config.FILES_TABLE_NAME}\`
+			SET 
+				\`chat_id\` = ?,
+				\`type\` = ?,
+				\`file_size\` = ?,
+				\`file_id\` = ?,
+				\`file_unique_id\` = ?,
+				\`height\` = ?,
+				\`width\` = ?,
+				\`tags\` = ?,
+				\`file_message_id\` = ?,
+				\`tags_message_id\` = ?,
+				\`media_group_id\` = ?,
+				\`used_count\` = 0,
+				\`is_deleted\` = 0
+		`,
+		[
+			chat_id,
+			type,
+			file_size,
+			file_id,
+			file_unique_id,
+			height,
+			width,
+			tags,
+			file_message_id,
+			tags_message_id,
+			media_group_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
 				}
 				else {
-					/*log.green("Insert:", {
-						table: table,
-						columns: columns,
-						results: results,
-					})*/
-					if (results.affectedRows > 0) {
-						resolve(true)
-					}
-					else {
-						resolve(false)
-					}
+					resolve(false)
 				}
 			}
-		)
-	})
-}
-
-const has = ({table, where}) => {
-	return new Promise((resolve, reject) => {
-		const keys = Object.keys(where)
-		DB.query(
-			`SELECT ${keys.join(", ")} FROM \`${table}\` WHERE ${keys.map(item => `\`${item}\` = ?`).join(" AND ")}`,
-			Object.values(where),
-			(error, results, fields) => {
-				if (error) {
-					log.red("Has error:", {
-						table: table,
-						where: where,
-						error: error,
-					})
-					reject(error)
-				}
-				else {
-					/*log.green("Has:", {
-						table: table,
-						where: where,
-						results: results,
-					})*/
-					resolve(results.length > 0)
-				}
-			}
-		)
-	})
-}
-
-const select = ({table, columns, where, order}) => {
-	return new Promise((resolve, reject) => {
-		let query = `SELECT ${columns.join(", ")} FROM \`${table}\``
-		
-		if (where) {
-			query += ` WHERE ${Object.keys(where).map(item => {
-				const operatorRegEx = /\[(.+)\]$/
-				if (operatorRegEx.test(item)) {
-					return `\`${item.replace(operatorRegEx, "")}\` ${item.match(operatorRegEx)[1]} ?`
-				}
-				else {
-					return `\`${item}\` = ?`
-				}
-			}).join(" AND ")}`
 		}
-		if (order) {
-			let column = Object.keys(order)[0]
-			query += ` ORDER BY \`${column}\` ${order[column].toUpperCase()}`
-		}
-		DB.query(
-			query,
-			where ? Object.values(where) : null,
-			(error, results, fields) => {
-				if (error) {
-					log.red("Select error:", {
-						table: table,
-						columns: columns,
-						where: where,
-						order: order,
-						error: error,
-					})
-					reject(error)
-				}
-				else {
-					/*log.green("Select:", {
-						table: table,
-						columns: columns,
-						where: where,
-						order: order,
-						results: results,
-					})*/
-					resolve(results)
-				}
-			}
-		)
-	})
-}
+	)
+})
 
-const update = ({table, columns, where}) => {
-	return new Promise((resolve, reject) => {
-		DB.query(
-			`UPDATE \`${table}\` SET ${Object.entries(columns).map(item => `\`${item[0]}\` = ?`).join(", ")} WHERE ${Object.keys(where).map(item => `\`${item}\` = ?`).join(" AND ")}`,
-			Object.values(columns).concat(Object.values(where)),
-			(error, results, fields) => {
-				if (error) {
-					log.red("Update eror:", {
-						table: table,
-						columns: columns,
-						where: where,
-						error: error,
-					})
-					reject(error)
+const createUser = ({
+	chat_id,
+	username = "",
+	first_name,
+	language_code = "",
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			INSERT INTO \`${config.USERS_TABLE_NAME}\`
+			SET 
+				\`chat_id\` = ?,
+				\`username\` = ?,
+				\`first_name\` = ?,
+				\`language_code\` = ?
+		`,
+		[
+			chat_id,
+			username,
+			first_name,
+			language_code,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
 				}
 				else {
-					/*log.green("Update:", {
-						table: table,
-						columns: columns,
-						where: where,
-						results: results,
-					})*/
-					if (results.affectedRows > 0) {
-						resolve(true)
-					}
-					else {
-						resolve(false)
-					}
+					resolve(false)
 				}
 			}
-		)
-	})
-}
+		}
+	)
+})
+
+const isFileExist = ({
+	chat_id,
+	message_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			SELECT
+				\`chat_id\`,
+				\`file_message_id\`
+			FROM \`${config.FILES_TABLE_NAME}\`
+			WHERE
+				\`chat_id\` = ? AND
+				(\`file_message_id\` = ? OR \`tags_message_id\` = ?)
+		`,
+		[
+			chat_id,
+			message_id,
+			message_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				resolve(results.length > 0)
+			}
+		}
+	)
+})
+
+const isUserExist = ({
+	chat_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			SELECT \`chat_id\`
+			FROM \`${config.USERS_TABLE_NAME}\`
+			WHERE
+				\`chat_id\` = ?
+		`,
+		[
+			chat_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				resolve(results.length > 0)
+			}
+		}
+	)
+})
+
+const getFile = ({
+	chat_id,
+	message_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			SELECT
+				\`id\`,
+				\`type\`,
+				\`chat_id\`,
+				\`file_id\`,
+				\`tags\`,
+				\`file_message_id\`,
+				\`used_count\`,
+				\`is_deleted\`,
+				\`media_group_id\`
+			FROM \`${config.FILES_TABLE_NAME}\`
+			WHERE
+				\`chat_id\` = ? AND
+				(\`file_message_id\` = ? OR \`tags_message_id\` = ?)
+		`,
+		[
+			chat_id,
+			message_id,
+			message_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				resolve(results[0])
+			}
+		}
+	)
+})
+
+const getUserFiles = ({
+	chat_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			SELECT
+				\`id\`,
+				\`type\`,
+				\`chat_id\`,
+				\`file_id\`,
+				\`tags\`,
+				\`file_message_id\`,
+				\`used_count\`,
+				\`is_deleted\`,
+				\`media_group_id\`
+			FROM \`${config.FILES_TABLE_NAME}\`
+			WHERE
+				\`chat_id\` = ?
+			ORDER BY \`date\` DESC
+		`,
+		[
+			chat_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				resolve(results)
+			}
+		}
+	)
+})
+
+const getUserFilesOfType = ({
+	chat_id,
+	type,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			SELECT
+				\`id\`,
+				\`type\`,
+				\`chat_id\`,
+				\`file_id\`,
+				\`tags\`,
+				\`file_message_id\`,
+				\`used_count\`,
+				\`is_deleted\`,
+				\`media_group_id\`
+			FROM \`${config.FILES_TABLE_NAME}\`
+			WHERE
+				\`chat_id\` = ? AND
+				\`type\` = ?
+			ORDER BY \`date\` DESC
+		`,
+		[
+			chat_id,
+			type,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				resolve(results)
+			}
+		}
+	)
+})
+
+const updateFileTags = ({
+	chat_id,
+	file_message_id,
+	tags,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			UPDATE \`${config.FILES_TABLE_NAME}\`
+			SET
+				\`tags\` = ?
+			WHERE
+				\`chat_id\` = ? AND
+				\`file_message_id\` = ?
+		`,
+		[
+			tags,
+			chat_id,
+			file_message_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
+				}
+				else {
+					resolve(false)
+				}
+			}
+		}
+	)
+})
+
+const updateMediaGroupTags = ({
+	chat_id,
+	media_group_id,
+	tags,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			UPDATE \`${config.FILES_TABLE_NAME}\`
+			SET
+				\`tags\` = ?
+			WHERE
+				\`chat_id\` = ? AND
+				\`media_group_id\` = ?
+		`,
+		[
+			tags,
+			chat_id,
+			media_group_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
+				}
+				else {
+					resolve(false)
+				}
+			}
+		}
+	)
+})
+
+const setFileDeletedState = ({
+	is_deleted,
+	chat_id,
+	file_message_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			UPDATE \`${config.FILES_TABLE_NAME}\`
+			SET
+				\`is_deleted\` = ?
+			WHERE
+				\`chat_id\` = ? AND
+				\`file_message_id\` = ?
+		`,
+		[
+			is_deleted,
+			chat_id,
+			file_message_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
+				}
+				else {
+					resolve(false)
+				}
+			}
+		}
+	)
+})
+
+const setMediaGroupDeletedState = ({
+	is_deleted,
+	chat_id,
+	media_group_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			UPDATE \`${config.FILES_TABLE_NAME}\`
+			SET
+				\`is_deleted\` = ?
+			WHERE
+				\`chat_id\` = ? AND
+				\`media_group_id\` = ?
+		`,
+		[
+			is_deleted,
+			chat_id,
+			media_group_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
+				}
+				else {
+					resolve(false)
+				}
+			}
+		}
+	)
+})
+
+const increaseUsedCount = ({
+	id,
+	chat_id,
+}) => new Promise((resolve, reject) => {
+	DB.query(
+		`
+			UPDATE \`${config.FILES_TABLE_NAME}\`
+			SET
+				\`used_count\` = \`used_count\` + 1
+			WHERE
+				\`id\` = ? AND
+				\`chat_id\` = ?
+		`,
+		[
+			id,
+			chat_id,
+		],
+		(error, results, fields) => {
+			if (error) {
+				reject(error)
+			}
+			else {
+				if (results.affectedRows > 0) {
+					resolve(true)
+				}
+				else {
+					resolve(false)
+				}
+			}
+		}
+	)
+})
+
 
 module.exports = {
-	select: select,
-	insert: insert,
-	has: has,
-	update: update,
+	saveFile,
+	createUser,
+	isFileExist,
+	isUserExist,
+	getFile,
+	getUserFiles,
+	getUserFilesOfType,
+	updateFileTags,
+	updateMediaGroupTags,
+	setFileDeletedState,
+	setMediaGroupDeletedState,
+	increaseUsedCount,
 }
