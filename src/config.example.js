468601306910140c1db3e380ede25690e6bb0bc4
Misha Saidov <mavlyd.saidov@gmail.com>
2021-06-28 05:08:21 +0600

    added example of config file

---
+++ src/config.example.js
@@ -0,0 +1,13 @@
+module.exports = ({
+	BOT_TOKEN: "1234567890:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	BOT_ID: 1234567890, //first numbers of BOT_TOKEN
+
+	BOT_USERNAME: "YourBotUsername",
+
+	MYSQL_HOST: "your_db_host",
+	MYSQL_USER: "your_db_user",
+	MYSQL_DATABASE: "your_db_name",
+
+	FILES_TABLE_NAME: "files",
+	USERS_TABLE_NAME: "users",
+});
