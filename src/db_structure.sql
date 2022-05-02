b1b0fff5aadba28a0d9fa52b5b8f67d8a2194c15
Миша <mavlyd.saidov@gmail.com>
2022-05-02 21:26:40 +0600

    db structure updated

--- src/db_structure.sql
+++ src/db_structure.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 28, 2021 at 12:50 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Хост: 127.0.0.1
+-- Время создания: Май 02 2022 г., 17:26
+-- Версия сервера: 10.1.34-MariaDB
+-- Версия PHP: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,21 +19,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `saved_img_bot`
+-- База данных: `saved_img_bot`
 --
+CREATE DATABASE IF NOT EXISTS `saved_img_bot` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `saved_img_bot`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `files`
+-- Структура таблицы `files`
 --
 
 CREATE TABLE `files` (
   `id` int(11) NOT NULL,
-  `chat_id` int(11) NOT NULL,
+  `chat_id` bigint(20) NOT NULL,
   `type` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
   `file_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_size` int(11) NOT NULL,
+  `file_size` bigint(20) NOT NULL,
   `file_unique_id` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `height` int(11) NOT NULL,
   `width` int(11) NOT NULL,
@@ -42,52 +45,53 @@ CREATE TABLE `files` (
   `used_count` int(11) NOT NULL,
   `is_deleted` int(11) NOT NULL,
   `media_group_id` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `chat_id` int(11) NOT NULL,
+  `chat_id` bigint(20) NOT NULL,
   `first_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `username` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `language_code` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
+  `is_premium` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- Индексы сохранённых таблиц
 --
 
 --
--- Indexes for table `files`
+-- Индексы таблицы `files`
 --
 ALTER TABLE `files`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Индексы таблицы `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT for table `files`
+-- AUTO_INCREMENT для таблицы `files`
 --
 ALTER TABLE `files`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
